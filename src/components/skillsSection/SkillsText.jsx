cb1687027962197cd25d33e1aa05ec1c91d5476b
rejrule <j.p2022p@gmail.com>
2025-04-23 13:04:45 -0400

    Updated frontend

--- src/components/skillsSection/SkillsText.jsx
+++ src/components/skillsSection/SkillsText.jsx
@@ -3,7 +3,7 @@
 const SkillsText = () => {
   return (
     <div className="flex flex-col items-center mt-[100px]">
-      <h2 className="text-6xl text-cyan mb-10">My Skills</h2>
+      <h2 className="text-6xl text-cyan mb-10">Front-End Development</h2>
       <p className="text-lg text-center">
         I not only work with these technologies but excellent in using them with
         best practices to deliver high-quality results, I have been working with
